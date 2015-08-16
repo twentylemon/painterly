cc799af40f2d06a9bf7484a3833196dcb0e1bd9a
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2015-08-16 19:19:25 -0400

    opencv rad mp4 working in test soln

--- painterly-rendering/painterly-rendering/main.cpp
+++ painterly-rendering/painterly-rendering/main.cpp
@@ -11,11 +11,17 @@ int main(int argc, char** argv) {
     g.window_width = 600;
     g.window_height = 600;
 
-    cv::Mat image = cv::imread("../images/chaika1.jpg", CV_LOAD_IMAGE_COLOR);
+    cv::Mat image = cv::imread("D:/pics/2.jpg", CV_LOAD_IMAGE_COLOR);
     cv::namedWindow("painterly rendering", cv::WINDOW_AUTOSIZE);
     cv::namedWindow("original", cv::WINDOW_AUTOSIZE);
-    cv::imshow("original", image);
+    
+    cv::VideoCapture cap("D:/vids/Game Of Thrones Opening 360p.mp4");
+    std::cout << cap.isOpened() << std::endl;
+    cap.set(CV_CAP_PROP_POS_MSEC, 4000);
+    cap >> image;
 
+    cv::imshow("original", image);
+    
     cv::GaussianBlur(image, image, cv::Size(3, 3), 0, 0, cv::BorderTypes::BORDER_DEFAULT);
     //cv::cvtColor(image, image, CV_RGB2GRAY);
 
@@ -27,7 +33,7 @@ int main(int argc, char** argv) {
 
     cv::addWeighted(grad_x, 0.5, grad_y, 0.5, 0, grad);
 
-    cv::imshow("painterly rendering", image);
+    cv::imshow("painterly rendering", grad);
 
     cv::waitKey();
     return 0;
